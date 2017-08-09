c23fbf030dc96e6750138b82d6b1f047a433fcce
Sam Foster <sfoster@mozilla.com>
2017-08-09 01:29:22 -0700

    Reworked preview tool. Added actually-works feature

--- preview.js
+++ preview.js
@@ -1,82 +1,305 @@
-var gSvgString;
+var gUniqueColors;
 var animationRAF;
-// the colors to match and replace in the svg
-let subsituteColors = {
-  fill: [],
-  stroke: []
-};
-// the colors we'll preview with
-let previewColors = {
-  fill: "#f00",
-  stroke: "#0f0"
-};
 
-let inputImg = document.querySelector("img") || document.querySelector("image");
-let outputDocument = null;
+// the colors we'll preview with that get matched and replaced in the svg
+let substituteColors = new Map([
+  ["context-fill", "#f00"],
+  ["context-stroke", "#0f0"]
+]);
 
-let params = new URLSearchParams(document.location.search.substring(1));
-console.log("URL params: ", params);
-if (params.has("src")) {
-  inputImg.src = params.get("src");
+let gInputDocument, gOutputDocument;
+
+async function main() {
+  let svgDocument = await loadInputImage();
+  gInputDocument = svgDocument;
+  gUniqueColors = getUniqueColorsFromDocument(gInputDocument);
+
+  console.log("got input document: ", gInputDocument);
+
+  await updateUI(gUniqueColors);
+  console.log("Input UI updated");
+
+  update(gInputDocument);
+  document.body.addEventListener("click", ui);
 }
 
-// get and process the image data
-let src = inputImg.src;
-console.log("Using image: ", src);
-fetch(src).then(resp => {
-  return resp.text();
-}).then(content => {
-  let parser = new DOMParser();
-  return parser.parseFromString(content.toString(), "image/svg+xml");
-}).then(svgDoc => {
-  if (svgDoc) {
-    console.log("got svgDoc: ", svgDoc);
-    outputDocument = svgDoc;
-    render(outputDocument);
-  } else {
-    console.log("what no svgDoc");
-  }
-});
+function update() {
+  let svgDocument = gInputDocument;
 
-function updateConfig() {
-  subsituteColors.fill = [];
-  subsituteColors.stroke = [];
-  ['fill', 'stroke'].forEach(name => {
-    let nodeId = name + 'Colors';
-    for (let value of document.getElementById(nodeId).value.split(';')) {
-      value = value.trim().toLowerCase();
-      if (value.startsWith('context-')) {
-        subsituteColors[name].push(value);
-      } else if (value.startsWith('rgb(')) {
-        subsituteColors[name].push(value);
-        try {
-          let hexColor = rgbToHex(value);
-          console.log('Converted %s to %s', value, hexColor);
-          subsituteColors[name].push(hexColor);
-        } catch(ex) {
-          console.log('Failed to convert %s to hex: %s\n%s', value, ex.name, ex.description);
-        }
-      } else if (value.startsWith('#')) {
-        subsituteColors[name].push(value);
-        try {
-          let rgbColor = hexToRgb(value);
-          console.log('Converted %s to %s', value, rgbColor);
-          subsituteColors[name].push(rgbColor);
-        } catch(ex) {
-          console.log('Failed to convert %s to rgb: %s\n%s', value, ex.name, ex.description);
+  let gOutputDocument = createOutputDocument(svgDocument);
+  renderPreview(gOutputDocument);
+  renderOutput(gOutputDocument);
+}
+
+let ui = {
+  handleEvent(evt) {
+    let target = evt.target;
+    // handle assignment checkbox clicks
+    if (target.type == "checkbox" && target.name.endsWith("-assignment")) {
+      let siblings = Array.from(target.parentNode.querySelectorAll("[type='checkbox']"))
+                      .filter(input => input !== target);
+      if (target.name == "none-assignment") {
+        // deselect the others
+        if (target.checked) {
+          siblings.forEach(input => {
+            input.checked = false;
+          });
         }
       } else {
-        subsituteColors[name].push(value);
+        siblings.forEach(input => {
+          if (input.name == "none-assignment") {
+            input.checked = false;
+          }
+        });
+      }
+    }
+  }
+}
+main();
+
+function getElementsWithAttribute(rootElement) {
+  let names = Array.from(arguments).slice(1);
+  let elements = [rootElement].concat(Array.from(rootElement.getElementsByTagName("*")));
+  let elementsWithAttribute = elements.filter(hasColor).filter(elm => {
+    return names.some(name => {
+      return hasAttributeWithValue(elm, name);
+    });
+  });
+  return elementsWithAttribute;
+}
+
+function hasAttributeWithValue(elm, name) {
+  let value = elm.getAttribute(name);
+  return (value && value.toLowerCase() !== "none");
+}
+
+function getColorsFromDocument(doc) {
+  let docElement = doc.documentElement;
+  let fillElements = getElementsWithAttribute(docElement, "fill");
+  let strokeElements = getElementsWithAttribute(docElement, "stroke");
+
+  let colorsFound = new Set();
+  let fillColors = new Map();
+  let strokeColors = new Map();
+
+  fillElements.forEach(elm => {
+    let color = elm.getAttribute("fill").toLowerCase();
+    let count = fillColors.get(color) || 0;
+    fillColors.set(color, ++count);
+    colorsFound.add(color);
+  });
+  strokeElements.forEach(elm => {
+    let color = elm.getAttribute("stroke").toLowerCase();
+    let count = strokeColors.get(color) || 0;
+    strokeColors.set(color, ++count);
+    colorsFound.add(color);
+  });
+  console.log("getColorsFromDocument: fillColors: ", fillColors);
+  console.log("getColorsFromDocument: strokeColors: ", strokeColors);
+  return colorsFound;
+}
+
+// ----------------------------------------
+// utils
+//
+function normalizeColorValue(color) {
+  let value = color.trim().toLowerCase();
+  if (value.startsWith('rgb(')) {
+    try {
+      let hexColor = rgbToHex(value);
+      return hexColor;
+    } catch(ex) {
+      console.warn('Failed to convert %s to hex: %s\n%s', value, ex.name, ex.description);
+      return value;
+    }
+  }
+  if (value.startsWith('#') && value.length == 4) {
+    value = "#" + value.substring(1).replace(/(.)/g, "$1$1");
+  }
+  return value;
+}
+
+function replaceColorsInDocument(doc, colorReplacementMap) {
+  console.log("replaceColorsInDocument got map: ", colorReplacementMap);
+  let colorElements = getElementsWithAttribute(doc.documentElement, "fill", "stroke");
+  colorElements.forEach(elm => {
+    if (hasAttributeWithValue(elm, "fill")) {
+      let color = elm.getAttribute("fill");
+      let normalizedColor = normalizeColorValue(color);
+      if (colorReplacementMap.has(normalizedColor)) {
+        // console.log("replace fill %s with %s", color, colorReplacementMap.get(normalizedColor));
+        elm.setAttribute("fill", colorReplacementMap.get(normalizedColor));
+      }
+    }
+    if (hasAttributeWithValue(elm, "stroke")) {
+      let color = elm.getAttribute("stroke");
+      let normalizedColor = normalizeColorValue(color);
+      if (colorReplacementMap.has(normalizedColor)) {
+        // console.log("replace stroke %s with %s", color, colorReplacementMap.get(normalizedColor));
+        elm.setAttribute("stroke", colorReplacementMap.get(normalizedColor));
       }
     }
   });
-  console.log('updateConfig, result: ', subsituteColors);
+  return doc;
+}
+
+function colorSwatchView(color) {
+  return `<div class="color-swatch">
+  <span class="color-preview" style="background-color: ${color}"></span>
+  <span class="color-label">${color}</span></div>`;
+}
+
+function colorAssignmentView(color) {
+  let swatch = colorSwatchView(color);
+  let fillChecked =  color == "context-fill" ? "checked" : "";
+  let strokeChecked =  color == "context-stroke" ? "checked" : "";
+  let noneChecked = !(fillChecked || strokeChecked);
+
+  return `<div class="color-assignment">
+    ${swatch}
+    <input type="checkbox" name="fill-assignment" ${fillChecked} value="${color}">
+    <input type="checkbox" name="stroke-assignment" ${strokeChecked} value="${color}">
+    <input type="checkbox" name="none-assignment" ${noneChecked} value="${color}">
+  </div>`;
+}
+
+// ----------------------------------------
+
+function loadInputImage() {
+  let inputImg = document.getElementById("input-image");
+  let params = new URLSearchParams(document.location.search.substring(1));
+  console.log("URL params: ", params);
+  if (params.has("src")) {
+    inputImg.src = params.get("src");
+  }
+  // get and process the image data
+  let src = inputImg.src;
+  console.log("Using image: ", src);
+  return fetch(src).then(resp => {
+    return resp.text();
+  }).then(content => {
+    let parser = new DOMParser();
+    return parser.parseFromString(content.toString(), "image/svg+xml");
+  })
+}
+
+function getUniqueColorsFromDocument(doc) {
+  let colorsFound = getColorsFromDocument(gInputDocument);
+  let uniqueColors = new Set();
+  for (let color of colorsFound) {
+    let ncolor = color && normalizeColorValue(color);
+    if (ncolor) {
+      uniqueColors.add(ncolor);
+    }
+  }
+  return uniqueColors;
+}
+
+function updateUI(colors) {
+  let colorHtmls = [];
+  for (let color of colors) {
+    let html = colorSwatchView(color);
+    colorHtmls.push(html);
+  }
+  document.getElementById("colors-found").insertAdjacentHTML("beforeend", colorHtmls.join("\n"));
+
+  let assignmentHtmls = [];
+  for (let color of colors) {
+    let html = colorAssignmentView(color);
+    assignmentHtmls.push(html);
+  }
+  document.getElementById("color-substitutes").insertAdjacentHTML("beforeend", assignmentHtmls.join("\n"));
+}
+
+function createOutputDocument(inputDocument) {
+  let outputDocument = inputDocument.cloneNode(inputDocument, true);
+  let replacements = new Map(substituteColors);
+  let fillColor = substituteColors.get("context-fill");
+  let strokeColor = substituteColors.get("context-stroke");
+  let checkedSelector = "#color-substitutes .color-assignment input[type='checkbox']:checked";
+
+  Array.from(document.querySelectorAll(checkedSelector)).forEach(input => {
+    switch (input.name) {
+      case "fill-assignment":
+        replacements.set(input.value, fillColor);
+        break;
+      case "stroke-assignment":
+        replacements.set(input.value, strokeColor);
+        break;
+      default:
+        console.log("Leave color: %s as-is", input.value);
+    }
+  });
+  replaceColorsInDocument(outputDocument, replacements);
+  return outputDocument;
+}
+
+function renderOutput(outputDocument, dataURI) {
+  if (!dataURI) {
+    let svgString = outputDocument.documentElement.outerHTML;
+    dataURI = "data:image/svg+xml;base64," +  btoa(svgString);
+  }
+  let outputContainer = document.querySelector("#output > .image-container");
+  let outputImg = new Image();
+  outputContainer.innerHTML = "";
+  outputContainer.appendChild(outputImg);
+  outputImg.src = dataURI;
+}
+
+function renderPreview(outputDocument, animationUrl) {
+  if (!animationUrl) {
+    let svgString = outputDocument.documentElement.outerHTML;
+    animationUrl = "data:image/svg+xml;base64," +  btoa(svgString);
+  }
+  let dims = getAnimationDimensions(outputDocument.documentElement);
+  console.log("getAnimationDimensions: ", dims);
+
+  let previewInner = document.getElementById("preview-inner");
+  let previewOuter = document.getElementById("preview-outer");
+  previewOuter.style.width = dims.frameWidth + "px";
+  previewOuter.style.height = dims.frameHeight + "px";
+  previewInner.style.width = dims.animationWidth + "px";
+  previewInner.style.height = dims.animationHeight + "px";
+
+  let backgroundImageValue = 'url(' + animationUrl + ')';
+  previewInner.style.backgroundImage = backgroundImageValue;
+  // console.log("value: ", backgroundImageValue);
+
+  let offsetX = 0;
+  let direction = -1;
+  function animate() {
+    let newOffsetX = offsetX + direction * dims.frameWidth;
+    if (Math.abs(newOffsetX) > dims.animationWidth) {
+      offsetX = 0; // direction *= -1;
+      newOffsetX = offsetX + direction * dims.frameWidth;
+    }
+    offsetX = newOffsetX;
+    previewInner.style.transform = `translateX(${offsetX}px)`;
+    animationRAF = requestAnimationFrame(animate);
+  }
+
+  // update keyframe animation
+  let frameCount = Math.floor(dims.animationWidth / dims.frameWidth) - 1;
+  let lastFrameX = dims.animationWidth - dims.frameWidth;
+  let rules = document.styleSheets[0].cssRules;
+  for (let rule of rules) {
+    if (rule.selectorText == '#preview-inner') {
+      rule.style['animation-duration'] = frameCount/60 +'s';
+      rule.style['animation-timing-function'] = `steps(${frameCount})`;
+      console.log('updated rule for ' + rule.selectorText + 'to: ' + rule.cssText);
+    }
+    if (rule instanceof CSSKeyframesRule) {
+      console.log("lastFrameX: ", lastFrameX);
+      console.log(rule.cssRules[1].style.transform = `translateX(-${lastFrameX}px)`);
+      console.log('updated last keyframe to: \n' + rule.cssText);
+    }
+  }
 }
 
 let SVG_SHAPES = {'rect': 1, 'path': 1, 'circle': 1, 'ellipse': 1, 'line': 1, 'polyline': 1, 'polygon': 1};
 
-function isShape(elm) {
-  return elm.localName in SVG_SHAPES;
+function hasColor(elm) {
+  return (elm.localName in SVG_SHAPES) || elm.hasAttribute("fill") || elm.hasAttribute("stroke");
 }
 
 function hexToRgb(str) {
@@ -94,22 +317,16 @@ function hexToRgb(str) {
 function rgbToHex(str) {
   let red=0, green=0, blue=0, alpha=1;
   let match;
-  if (match = str.trim().match(/rgb\((\d+),\s*(\d+), ,\s*(\d+)\)/)) {
-    red = match[1];
-    green = match[2];
-    blue = match[3];
+  if (match = str.trim().match(/\d+\%?/g)) {
+    [red, green, blue, alpha] = Array.from(match).map(color => {
+      if (color.endsWith("%")) {
+        return 255 * Number(color.substring(0, color.length-1)) / 100;
+      }
+      return Number(color);
+    });
   }
 
-  const isPercent = (red + (alpha || '')).toString().includes('%');
-
-  if (typeof red === 'string') {
-    const res = red.match(/(0?\.?\d{1,3})%?\b/g).map(Number);
-    // TODO: use destructuring when targeting Node.js 6
-    red = res[0];
-    green = res[1];
-    blue = res[2];
-    alpha = res[3];
-  } else if (alpha !== undefined) {
+  if (alpha !== undefined) {
     alpha = parseFloat(alpha);
   }
 
@@ -158,138 +375,4 @@ function getAnimationDimensions(svgElement) {
     dims.frameHeight = Math.min(parseFloat(frame2.getAttribute("y")), dims.frameHeight);
   }
   return dims;
-}
-
-function render(svgDoc) {
-  if (animationRAF) {
-    cancelAnimationFrame(animationRAF);
-  }
-  let docElement;
-  let outputContainer = document.getElementById("output");
-  if (svgDoc) {
-    while (outputContainer.firstChild) {
-      outputContainer.removeChild(outputContainer.firstChild);
-    }
-    outputContainer.appendChild(svgDoc.documentElement);
-    let serializer = new XMLSerializer();
-    gSvgString = serializer.serializeToString(svgDoc);
-  }
-  docElement = outputContainer.firstChild;
-
-  if (!docElement) {
-    console.warn("No document to render");
-    return;
-  }
-
-  updateConfig();
-
-  window.gSVGDocumentElement = docElement;
-
-  let colorsFound = {
-    fill: new Map(),
-    stroke: new Map()
-  };
-
-  for(let elm of docElement.getElementsByTagName("*")) {
-    if (isShape(elm)) {
-      let stroke = elm.hasAttribute("stroke") ? elm.getAttribute("stroke").toLowerCase() : null;
-      if (stroke == "none") {
-        stroke = null;
-      }
-      let fill = elm.hasAttribute("fill") ? elm.getAttribute("fill").toLowerCase() : null;
-      if (fill == "none") {
-        fill = null;
-      }
-      if (stroke)
-      {
-        if (subsituteColors.stroke.includes(stroke)) {
-          elm.setAttribute("stroke", previewColors.stroke);
-        } else if(subsituteColors.fill.includes(fill)) {
-          elm.setAttribute("stroke", previewColors.fill);
-        }
-        let count = colorsFound.stroke.get(stroke) || 0;
-        colorsFound.stroke.set(stroke, ++count);
-      }
-      if (fill)
-      {
-        if (subsituteColors.fill.includes(fill)) {
-          elm.setAttribute("fill", previewColors.fill);
-        } else if(subsituteColors.stroke.includes(fill)) {
-          elm.setAttribute("fill", previewColors.stroke);
-        }
-        let count = colorsFound.fill.get(fill) || 0;
-        colorsFound.fill.set(fill, ++count);
-      }
-    }
-  };
-
-  function renderColorsFound(colorCounts) {
-    let colors = [];
-    for(let [name, count] of colorCounts) {
-      colors.push(`${name} (${count})`);
-    }
-    return colors.join(", ");
-  }
-
-  document.getElementById("preview-fill").style.backgroundColor = previewColors.fill;
-  document.getElementById("preview-stroke").style.backgroundColor = previewColors.stroke;
-  document.getElementById("fill-colors-found").textContent = renderColorsFound(colorsFound.fill);
-  document.getElementById("stroke-colors-found").textContent = renderColorsFound(colorsFound.stroke);
-
-  let dims = getAnimationDimensions(docElement);
-  console.log("getAnimationDimensions: ", dims);
-
-  let previewInner = document.getElementById("preview-inner");
-  let previewOuter = document.getElementById("preview-outer");
-  previewOuter.style.width = dims.frameWidth + "px";
-  previewOuter.style.height = dims.frameHeight + "px";
-  previewInner.style.width = dims.animationWidth + "px";
-  previewInner.style.height = dims.animationHeight + "px";
-
-  // let svgString = docElement.outerHTML;
-  let parser = new DOMParser();
-  console.log("outerHTML: ", btoa(docElement.outerHTML));
-  let svgString = docElement.outerHTML;
-  // let tmpDoc = parser.parseFromString(docElement.outerHTML, "image/svg+xml");
-  // var serializer = new XMLSerializer();
-  // var svgString = serializer.serializeToString(tmpDoc);
-  // console.log("serialized: ", btoa(svgString));
-
-  let exampleSVG = "PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxNiIgaGVpZ2h0PSIxNiIgdmlld0JveD0iMCAwIDE2IDE2Ij4KICA8cGF0aCBkPSJNOCwxMkwzLDcsNCw2bDQsNCw0LTQsMSwxWiIgZmlsbD0iIzZBNkE2QSIgLz4KPC9zdmc+Cg==";
-  // console.log("animationUrl: ", animationUrl);
-  let animationUrl = "data:image/svg+xml;base64," +  btoa(svgString);
-  let backgroundImageValue = 'url(' + animationUrl + ')';
-  previewInner.style.backgroundImage = backgroundImageValue;
-  // console.log("value: ", backgroundImageValue);
-
-  let offsetX = 0;
-  let direction = -1;
-  function animate() {
-    let newOffsetX = offsetX + direction * dims.frameWidth;
-    if (Math.abs(newOffsetX) > dims.animationWidth) {
-      offsetX = 0; // direction *= -1;
-      newOffsetX = offsetX + direction * dims.frameWidth;
-    }
-    offsetX = newOffsetX;
-    previewInner.style.transform = `translateX(${offsetX}px)`;
-    animationRAF = requestAnimationFrame(animate);
-  }
-
-  // update keyframe animation
-  let frameCount = Math.floor(dims.animationWidth / dims.frameWidth) - 1;
-  let lastFrameX = dims.animationWidth - dims.frameWidth;
-  let rules = document.styleSheets[0].cssRules;
-  for (let rule of rules) {
-    if (rule.selectorText == '#preview-inner') {
-      rule.style['animation-duration'] = frameCount/60 +'s';
-      rule.style['animation-timing-function'] = `steps(${frameCount})`;
-      console.log('updated rule for ' + rule.selectorText + 'to: ' + rule.cssText);
-    }
-    if (rule instanceof CSSKeyframesRule) {
-      console.log("lastFrameX: ", lastFrameX);
-      console.log(rule.cssRules[1].style.transform = `translateX(-${lastFrameX}px)`);
-      console.log('updated last keyframe to: \n' + rule.cssText);
-    }
-  }
-
 }
